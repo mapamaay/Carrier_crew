f66f1291adc8cb1a2ff11f0e75357709f9289988
Malo Ayral <malo.ayral@gmail.com>
2020-06-11 23:47:30 +0800

    Work from G_Romain and L_Tom added

---
+++ Project_Carrier_crew/Assets/Scripts/Scripts_L_Tom/EventsManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventsManager : MonoBehaviour
+{
+    public static EventsManager current;
+    public event Action onPlayer1Interaction;
+    public event Action onPlayer2Interaction;
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+    public void Player1Interaction()
+    {
+        onPlayer1Interaction?.Invoke();
+    }
+    
+    public void Player2Interaction()
+    {
+        onPlayer2Interaction?.Invoke();
+    }
+}
