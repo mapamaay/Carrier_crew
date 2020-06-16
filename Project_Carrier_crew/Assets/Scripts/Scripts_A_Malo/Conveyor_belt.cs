ff4ba6a90d5794527106f91cff0bb1727566eb84
Malo Ayral <malo.ayral@gmail.com>
2020-06-16 23:11:03 +0800

    New scene A_Malo with script

---
+++ Project_Carrier_crew/Assets/Scripts/Scripts_A_Malo/Conveyor_belt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conveyor_belt : MonoBehaviour
+{
+    public GameObject belt;
+    public Transform endpoint;
+    public float speed;
+
+    void OnTriggerStay(Collider other)
+    {
+        other.transform.position = Vector3.MoveTowards(other.transform.position, endpoint.position, speed * Time.deltaTime);
+    }
+
+ 
+}
