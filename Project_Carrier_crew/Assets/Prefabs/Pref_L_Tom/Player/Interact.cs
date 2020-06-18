2dd44229576cb8a1791204d0320731729237f12f
Malo Ayral <malo.ayral@gmail.com>
2020-06-18 19:29:49 +0800

    Folder renamming, conveyor belt finished

---
+++ Project_Carrier_crew/Assets/Prefabs/Pref_L_Tom/Player/Interact.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Interact : MonoBehaviour
+{
+    public GameObject Poutre;
+    public event Action OnInteraction;
+
+
+    private void Awake()
+    {
+        if (this.CompareTag("Player 1"))
+        {
+            
+        }
+        else
+        {
+            
+        }
+    }
+
+    private void OnInteract()
+    {
+        OnInteraction?.Invoke();
+    }
+
+    private void Testing_OnInteractionPressed(object sender, EventArgs e)
+    {
+        Debug.Log("Interacting!");
+    }
+    void Update()
+    {
+        
+    }
+}
